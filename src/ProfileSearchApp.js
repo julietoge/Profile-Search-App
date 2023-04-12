14170bf0a8cf6c0163686a66a67539898ef0a3ca
julietoge <julietogechi27@gmail.com>
2023-04-12 19:18:36 +0100

    second commit

--- src/ProfileSearchApp.js
+++ src/ProfileSearchApp.js
@@ -5,17 +5,19 @@ const ProfileApp = () => {
   const peopleItems = users.map((person, index) => {
     return (
       <p>
-      <span key={person.id}>{person.title}</span>&nbsp;
-      <span key={person.id}>{person.firstName}</span>
-      <span key={person.id}>{person.lastName}</span>
-      <img src="{person.picture}" alt="pic" style={{width: "32px", height: "32px"}} />
-    </p>
-    )
+        <span key={person.id}>{person.title}</span>&nbsp;
+        <span key={person.id}>{person.firstName}</span>
+        <span key={person.id}>{person.lastName}</span>
+        <img
+          src={person.picture}
+          alt="pic"
+          style={{ width: "32px", height: "32px" }}
+        />
+      </p>
+    );
   });
 
-  return (
-    <div>{peopleItems}</div>
-  );
-}
+  return <div>{peopleItems}</div>;
+};
 
 export default ProfileApp;
